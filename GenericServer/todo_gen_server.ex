161da3d705265514acedf28274946eed7d213ccb
Alioune Gaye <alioune.gaye@secomind.com>
2025-07-22 16:29:30 +0200

    Genserver Todo List

---
+++ GenericServer/todo_gen_server.ex
@@ -0,0 +1,181 @@
+
+defmodule TodoGenServer do
+  use GenServer
+
+  @name :todo_server
+
+  ## === API pubblica ===
+
+  def start() do
+    GenServer.start(__MODULE__, nil) |> register()
+  end
+
+  defp register({:ok, pid}) do
+    Process.register(pid, @name)
+    {:ok, pid}
+  end
+
+  def add_entry(entry) do
+    GenServer.cast(@name, {:add_entry, entry})
+  end
+
+  def entries(date) do
+    GenServer.call(@name, {:entries, date})
+  end
+
+  def update_entry(id, updater_fun) do
+    GenServer.cast(@name, {:update_entry, id, updater_fun})
+  end
+
+  def delete_entry(id) do
+    GenServer.cast(@name, {:delete_entry, id})
+  end
+
+  ## === Callback GenServer ===
+
+  @impl true
+  def init(_args) do
+    {:ok, TodoList.new()}
+  end
+
+  @impl true
+  def handle_cast({:add_entry, entry}, state) do
+    new_state = TodoList.add_entry(state, entry)
+    {:noreply, new_state}
+  end
+
+  @impl true
+  def handle_cast({:update_entry, id, fun}, state) do
+    new_state = TodoList.update_entry(state, id, fun)
+    {:noreply, new_state}
+  end
+
+  @impl true
+  def handle_cast({:delete_entry, id}, state) do
+    new_state = TodoList.delete_entry(state, id)
+    {:noreply, new_state}
+  end
+
+  @impl true
+  def handle_call({:entries, date}, _from, state) do
+    {:reply, TodoList.entries(state, date), state}
+  end
+end
+
+defmodule TodoList do
+  defstruct next_id: 1, entries: %{}
+
+  def new(entries \\ []) do
+    Enum.reduce(
+      entries,
+      %TodoList{},
+      # &add_entry(&2, &1)
+      fn entry, todo_list_acc ->
+        add_entry(todo_list_acc, entry)
+      end
+    )
+  end
+
+  # def new(), do: %TodoList{}
+
+  # def new(), do: MultiDict.new()
+
+  def add_entry(todo_list, entry) do
+    # update the entry’s id value with the value stored in the next_id
+    entry = Map.put(entry, :id, todo_list.next_id)
+
+    new_entries =
+      Map.put(
+        todo_list.entries,
+        todo_list.next_id,
+        entry
+      )
+
+    %TodoList{todo_list | entries: new_entries, next_id: todo_list.next_id + 1}
+  end
+
+  def entries(todo_list, date) do
+    todo_list.entries
+    |> Map.values()
+    |> Enum.filter(&(&1.date == date))
+  end
+
+  # I(l terzo argomento è la lamba che mi dice cosa fare con il vecchio entry
+  # Il suo risultato diventa il nuovo entry che poi metto nella set degli altri
+  def update_entry(todo_list, entry_id, updater_fun) do
+    case Map.fetch(todo_list.entries, entry_id) do
+      :error ->
+        todo_list
+
+      {:ok, old_entry} ->
+        new_entry = updater_fun.(old_entry)
+        new_entries = Map.put(todo_list.entries, new_entry.id, new_entry)
+        %TodoList{todo_list | entries: new_entries}
+    end
+  end
+
+  def delete_entry(todo_list, entry_id) do
+    case Map.has_key?(todo_list.entries, entry_id) do
+      false ->
+        todo_list
+
+      true ->
+        new_entries = Map.delete(todo_list.entries, entry_id)
+        %TodoList{todo_list | entries: new_entries}
+    end
+  end
+
+  defmodule CsvImporter do
+    def import(path) do
+      path
+      |> File.stream!()
+      |> IO.inspect()
+      |> Stream.map(&String.trim_trailing(&1, "\n"))
+      |> IO.inspect()
+      |> Stream.map(&parse_line/1)
+      |> IO.inspect()
+      |> Enum.to_list()
+      |> IO.inspect()
+      |> TodoList.new()
+    end
+
+    defp parse_line(line) do
+      [date, title] = String.split(line, ",")
+      %{date: Date.from_iso8601!(date), title: title}
+    end
+  end
+end
+
+defmodule MultiDict do
+  def new(), do: %{}
+
+  def add(dict, key, value) do
+    Map.update(dict, key, [value], &[value | &1])
+  end
+
+  def get(dict, key) do
+    Map.get(dict, key, [])
+  end
+end
+
+defimpl Collectable, for: TodoServ do
+  def into(original) do
+    {original, &into_callback/2}
+  end
+
+  defp into_callback(todo_list, {:cont, entry}) do
+    TodoList.add_entry(todo_list, entry)
+  end
+
+  defp into_callback(todo_list, :done), do: todo_list
+  defp into_callback(_todo_list, :halt), do: :ok
+end
+
+
+####---------------TEst----------------------------
+#TodoGenServer.start()
+#TodoGenServer.add_entry(%{date: ~D[2025-07-22], title: "Scrivere GenServer"})
+#TodoGenServer.entries(~D[2025-07-22])
+#TodoGenServer.update_entry(1, fn e -> %{e | title: "Aggiornato"} end)
+#TodoGenServer.delete_entry(1)
+
